487b8891c99ce121658f3b68eb82abeac8e77e4c
Hamza-777 <hamzararrani77@gmail.com>
2021-04-10 06:16:25 +0530

    update

--- app.js
+++ app.js
@@ -1002,3 +1002,94 @@
 // while (match = number.exec(input)) {
 // console.log("Found", match[0], "at", match.index);
 // }
+
+// ==================================================
+// Basic Problem 30 (Program to parse INI type files)
+// ==================================================
+
+// function parseINI(string) {
+//     // Start with an object to hold the top-level fields
+//     let result = {};
+//     let section = result;
+//     string.split(/\r?\n/).forEach(line => {
+//         let match;
+//         if (match = line.match(/^(\w+)=(.*)$/)) {
+//             section[match[1]] = match[2];
+//         } else if (match = line.match(/^\[(.*)\]$/)) {
+//             section = result[match[1]] = {};
+//         } else if (!/^\s*(;.*)?$/.test(line)) {
+//             throw new Error("Line '" + line + "' is not valid.");
+//         }
+//     });
+//     return result;
+// }
+
+// console.log(parseINI(`
+// name=Vasilis
+// [address]
+// city=Tessaloniki`));
+
+// =================================================================
+// Basic Problem 31 (Program to check occurence of given substrings)
+// =================================================================
+
+// let vehicle = /ca(r|t)/;
+// let vehicle = /ca[rt]/;
+
+// console.log(vehicle.test('high art'));
+
+// let pop = /pr?op/;
+
+// console.log(pop.test('prrrop'));
+
+// let ferr = /ferr(et|y|ari)/;
+
+// console.log(ferr.test('transfer A'));
+
+// let ious = /ious\b/;
+
+// console.log(ious.test('spacious room'));
+
+// let space = /\s(\.|,|:|;)/;
+// let space = /\s[.,:;]/;
+
+// console.log(space.test('bad punctuation :'));
+
+// let morethan7 = /\w{7,}/;
+// let morethan7 = /\w{7}/;
+
+// console.log(morethan7.test('three small words'));
+
+// let noe = /\b[^\We]+\b/i;
+
+// console.log(noe.test('red platypus'));
+
+// ==============================================================================
+// Basic Problem 32 (Program to replace single quotations with double quotations)
+// ==============================================================================
+
+// let text = "'I'm the cook,' he said, 'it's my job.'";
+
+// console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
+
+// console.log(text.match(/(^|\W)'|'(\W|$)/g));
+
+// =========================================================
+// Basic Problem 33 (Program to identify javascript numbers)
+// =========================================================
+
+// let number = /^[+\-]?(\d+(\.\d*)?|\.\d+)([eE][+\-]?\d+)?$/;
+
+// // Tests:
+// for (let str of ["1", "-1", "+15", "1.55", ".5", "5.",
+//                  "1.3e2", "1E-4", "1e+12"]) {
+//   if (!number.test(str)) {
+//     console.log(`Failed to match '${str}'`);
+//   }
+// }
+// for (let str of ["1a", "+-1", "1.2.3", "1+1", "1e4.5",
+//                  ".5.", "1f5", "."]) {
+//   if (number.test(str)) {
+//     console.log(`Incorrectly accepted '${str}'`);
+//   }
+// }
